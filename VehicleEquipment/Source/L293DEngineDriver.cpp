cf4c89ef8effeaa956a999fd254b4cad3fc9dc5d
marcinwozniak0 <wozniak1008@gmail.com>
2019-05-20 22:04:32 +0200

    Add checking correctness of coordinates range in L293DEngineDriver

--- VehicleEquipment/Source/L293DEngineDriver.cpp
+++ VehicleEquipment/Source/L293DEngineDriver.cpp
@@ -1,6 +1,6 @@
 #include "L293DEngineDriver.hpp"
 #include "VehicleConfiguration.hpp"
-#include <iostream>
+
 std::array<std::array<uint8_t, NUMBER_OF_PINS_PER_ENGINE>, NUMBER_OF_ENGINES>
 L293DEngineDriver::calculatePinValues(const std::pair<int16_t, int16_t>& characteristic) const
 {
@@ -10,7 +10,7 @@ L293DEngineDriver::calculatePinValues(const std::pair<int16_t, int16_t>& charact
     const uint8_t pwmValue = PWM_MAX_RANGE * characteristic.second / EXTERNAL_INTERFACES::COORDINATE_SYSTEM_RESOLUTION;
     constexpr uint8_t directionChangeTreshold = 0;
 
-    if (directionChangeTreshold < characteristic.second)
+    if (areCoordinatesInRange(characteristic) and directionChangeTreshold < characteristic.second)
     {
          pinValuesOfLeftEngine = {_drivingForward.at(0).at(0),
                                   _drivingForward.at(0).at(1),
@@ -21,7 +21,7 @@ L293DEngineDriver::calculatePinValues(const std::pair<int16_t, int16_t>& charact
                                   pwmValue};
 
     }
-    else if (directionChangeTreshold > characteristic.second)
+    else if (areCoordinatesInRange(characteristic) and directionChangeTreshold > characteristic.second)
     {
          pinValuesOfLeftEngine = {_drivingBackward.at(0).at(0),
                                   _drivingBackward.at(0).at(1),
@@ -38,4 +38,14 @@ L293DEngineDriver::calculatePinValues(const std::pair<int16_t, int16_t>& charact
     }
 
     return {pinValuesOfLeftEngine, pinValuesOfRightEngine};
+}
+
+bool L293DEngineDriver::areCoordinatesInRange(const std::pair<int16_t, int16_t>& coordinates) const
+{
+    const auto& [xCoordinate, yCoordinate] = coordinates;
+
+    return xCoordinate <= EXTERNAL_INTERFACES::COORDINATE_SYSTEM_RESOLUTION and
+           yCoordinate <= EXTERNAL_INTERFACES::COORDINATE_SYSTEM_RESOLUTION and
+           xCoordinate >= -EXTERNAL_INTERFACES::COORDINATE_SYSTEM_RESOLUTION and
+           yCoordinate >= -EXTERNAL_INTERFACES::COORDINATE_SYSTEM_RESOLUTION;
 }
