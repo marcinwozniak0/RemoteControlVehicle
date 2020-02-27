583b36cf2cd01ac22d376c93de56a83e03cceb79
marcinwozniak0 <wozniak1008@gmail.com>
2020-02-27 17:55:59 +0100

    Moved building artefacts destination folder to build directory

--- scripts/ci_scripts/compile_project_ci.sh
+++ scripts/ci_scripts/compile_project_ci.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /builds/marcinwozniak0/RemoteControlVehicle/CMake
 mkdir build
+cd build
 git clone -b v1.25.0 https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
-cd ../build
+cd ..
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j8
