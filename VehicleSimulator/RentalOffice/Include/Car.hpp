35320642e741e7756925b1e5093b362c20b9d045
marcinwozniak0 <wozniak1008@gmail.com>
2020-11-29 20:41:51 +0100

    Added _id and less comparator to Car structure

--- VehicleSimulator/RentalOffice/Include/Car.hpp
+++ VehicleSimulator/RentalOffice/Include/Car.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
 struct Car
-{};
+{
+    Car(const uint id) : _id(id)
+    {}
+
+    bool operator< (const Car& car) const
+    {
+        return _id < car._id;
+    }
+
+    const uint _id;
+};
